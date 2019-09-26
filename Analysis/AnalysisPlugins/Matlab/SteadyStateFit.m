58d85866b4d6edd668211518724d56761693ecdd
Marcelo <mbrittom@gmail.com>
2019-09-26 10:19:43 -0400

    Fixed issue in SteadyStateFit.m where frequency was the average of all 6 input channels and testing was done only with 3-phase voltage.  Also optimized the matlab routine.

--- Analysis/AnalysisPlugins/Matlab/SteadyStateFit.m
+++ Analysis/AnalysisPlugins/Matlab/SteadyStateFit.m
@@ -8,13 +8,6 @@ function [Synx,Freq,ROCOF] = SteadyStateFit ( ...
 	Samples ...
 )
 
-%dummy implementation
-% Ain = [70,70,70,5,5,5];
-% Theta = [0, -(2/3)*pi, (2/3)*pi, 0, -(2/3)*pi, (2/3)*pi,];
-% Synx = (Ain.*exp(-1i.*Theta)).';
-% % Freq = 60;
-% ROCOF = 0;
-
 %SignalParams: array of doubles
 % For Steady State signals:
 % LV:Mat
@@ -59,21 +52,22 @@ ROCOFs(1:NPhases) = 0;
 %time-base
 dt = 1/SampleRate;
 %tn = (0:NSamples-1)*dt;
-tn = (-NSamples/2:NSamples/2-1)*dt;
-
-MaxIter = 50;
+%tn = (-NSamples/2:NSamples/2-1)*dt;
+tn = (-((NSamples/2)-(1/2)):((NSamples/2)-(1/2)))*dt;
 FitCrit = 1e-5;   
-dFreq(NPhases,MaxIter) = 0;
-erms(1:NPhases) = 1;
+MaxIter = 50;
+% pre-allocate variables for speed
+dFreq = zeros(NPhases,MaxIter);
+A = zeros(MaxIter,1);
+B = zeros(MaxIter,1);
+%Ain = zeros(NPhases,1);
+%Theta = zeros(NPhases,1);
+AinH = zeros(NPhases,1);
+ThetaH = zeros(NPhases,1);
 
-for p = 1:NPhases
-    
-    
-    
+for p = 1:NPhases    
     %Pre-fit: generate the model using first estimated frequency
-
-    H = [ones(1,NSamples)' cos(2*pi*FundFrequency*tn)' sin(2*pi*FundFrequency*tn)' ];
-
+    H = [cos(2*pi*FundFrequency*tn)' sin(2*pi*FundFrequency*tn)' ones(1,NSamples)'];
     if NHarm>1
         H = [H cos(2*pi*fh*tn)' sin(2*pi*fh*tn)'];
     end
@@ -81,40 +75,41 @@ for p = 1:NPhases
     %traditional least squares linear fit  - LV uses SVD
     % fitting function: x[n] = Vdc + A*cos(2*pi*f*tn) + B*sin(2*pi*f*tn)
     %                              + C*cos(2*pi*fh*tn) + D*sin(2*pi*fh*tn)
-    S = inv(H'*H)*(H'*Samples(p,:)');
-    Vdc(1) = S(1); A(1) = S(2); B(1) = S(3);
-    Ain(p) = sqrt(A(1)^2 + B(1)^2)*MagCorr(p);
-    Theta(p) = atan2(B(1),A(1)) + DelayCorr(p)*1e-9*2*pi*Freq; 
+    %S = inv(H'*H)*(H'*Samples(p,:)');  %Matlab warns that inv(A)*b is less accurate and efficient than A\b
+    S = (H'*H)\(H'*Samples(p,:)');
+    A(1) = S(1); B(1) = S(2);
+    %Ain(p) = sqrt(A(1)^2 + B(1)^2)*MagCorr(p);
+    %Theta(p) = atan2(B(1),A(1)) + DelayCorr(p)*1e-9*2*pi*Freq; 
 
     if NHarm>1
         C(1) = S(4); D(1) = S(5);
-        AinH(p) = sqrt(C(1)^2 + D(1)^2)*MagCorr(p);
-        ThetaH(p) = atan2(D(1),C(1)) + DelayCorr(p)*1e-9*2*pi*fh;
-    else
-        AinH(p) = 0;
-        ThetaH(p) = 0;
+        %AinH(p) = sqrt(C(1)^2 + D(1)^2)*MagCorr(p);
+        %ThetaH(p) = atan2(D(1),C(1)) + DelayCorr(p)*1e-9*2*pi*fh;
+    %else
+        %AinH(p) = 0;
+        %ThetaH(p) = 0;
     end
     
     for k = 1:MaxIter
         %Four parameter iterative fit
         % update model -- adding frequency variation model
-        H = [ones(1,NSamples)' cos(2*pi*Freqs(p)*tn)' sin(2*pi*Freqs(p)*tn)'];
+        H = [cos(2*pi*Freqs(p)*tn)' sin(2*pi*Freqs(p)*tn)' ones(1,NSamples)'];
         if NHarm>1
             H = [H cos(2*pi*fh*tn)' sin(2*pi*fh*tn)'];
         end
         G = [H (-A(k)*tn.*sin(2*pi*Freqs(p)*tn) + B(k)*tn.*cos(2*pi*Freqs(p)*tn))'];
-        S = inv(G'*G)*(G'*Samples(p,:)');
-       
-        A(k+1) = S(2); B(k+1) = S(3); 
-        Ain(p) = sqrt(A(k+1)^2 + B(k+1)^2)*MagCorr(p);
-        Theta(p) = atan2(B(k+1),A(k+1)) + DelayCorr(p)*1e-9*2*pi*Freq; 
+        %S = inv(G'*G)*(G'*Samples(p,:)'); %Matlab warns that inv(A)*b is less accurate and efficient than A\b
+        S = (G'*G)\(G'*Samples(p,:)');
+        A(k+1) = S(1); B(k+1) = S(2); 
+        %Ain(p) = sqrt(A(k+1)^2 + B(k+1)^2)*MagCorr(p);
+        %Theta(p) = atan2(B(k+1),A(k+1)) + DelayCorr(p)*1e-9*2*pi*Freq; 
         if NHarm>1
             C(1) = S(4); D(1) = S(5);
-            AinH(p) = sqrt(C(1)^2 + D(1)^2)*MagCorr(p);
-            ThetaH(p) = atan2(D(1),C(1)) + DelayCorr(p)*1e-9*2*pi*fh;
+            %AinH(p) = sqrt(C(1)^2 + D(1)^2)*MagCorr(p);
+            %ThetaH(p) = atan2(D(1),C(1)) + DelayCorr(p)*1e-9*2*pi*fh;
         else
-            AinH(p) = 0;
-            ThetaH(p) = 0;
+            %AinH(p) = 0;
+            %ThetaH(p) = 0;
         end
         dFreq(p,k) = S(size(S,1))/(2*pi);
         Freqs(p) = Freqs(p) + dFreq(p,k);
@@ -127,6 +122,15 @@ for p = 1:NPhases
             break
         end
     end
+    Ain(p) = sqrt(A(k+1)^2 + B(k+1)^2)*MagCorr(p);
+    Theta(p) = atan2(B(k+1),A(k+1)) + DelayCorr(p)*1e-9*2*pi*Freq;
+    if NHarm > 1
+        AinH(p) = sqrt(C(1)^2 + D(1)^2)*MagCorr(p);
+        ThetaH(p) = atan2(D(1),C(1)) + DelayCorr(p)*1e-9*2*pi*fh;        
+    else
+        AinH(p) = 0;
+        ThetaH(p) = 0;
+    end
 end
 
 % for p=1:NPhases
@@ -135,17 +139,16 @@ end
 
 %Fit - magnitude and phase
 Synx = (Ain/sqrt(2).*exp(-1i.*Theta)).';
-% Synx = (vrms.*exp(1i.*Theta)).';
 
-%Calculating simmetrical components
+%Calculating symmetrical components
 alfa = exp(2*pi*1i/3);
 Ai = (1/3)*[1 1 1; 1 alfa alfa^2; 1 alfa^2 alfa];
 
 Vabc = Synx(1:3,:);
-Iabc = Synx(4:6,:);
+Vzpn = Ai*Vabc; %voltage: zero, positive and negative sequence
 
-Vzpn = Ai*Vabc; %voltage: zero, positive and negative
-Izpn = Ai*Iabc;
+Iabc = Synx(4:6,:);
+Izpn = Ai*Iabc; %curren: zero, positive and negative sequence
 
 %Synx output:
 Synx = [ Vabc.' Vzpn(2) Iabc.' Izpn(2)];
@@ -153,10 +156,5 @@ Synx = [ Vabc.' Vzpn(2) Iabc.' Izpn(2)];
 %AinH = AinH./Ain  %not used yet, but should be output to verify a calibrator
 %ThetaH
 
-%Freqs
-%ROCOFs
-%k
-
-Freq = mean(Freqs);
-ROCOF = mean(ROCOFs);
-%Synx(7) = erms(1); %just to inform the final residual to LV
+Freq = mean(Freqs(1:3)); % average of the voltage frequencies 
+ROCOF = mean(ROCOFs(1:3));
